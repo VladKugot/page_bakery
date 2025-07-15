71d9f42f25910a9b34df5f8c58a5a958c6fca7cd
Vlad <vladkygot@gmail.com>
2025-07-15 16:08:24 +0300

    make build

---
+++ index.afd31956.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(":root");document.querySelector("#dark").addEventListener("click",function(){event.preventDefault(),e.classList.toggle("dark")});
+//# sourceMappingURL=index.afd31956.js.map
